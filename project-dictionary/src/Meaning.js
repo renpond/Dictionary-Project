5fd81f9416f0da2e0a7a89b922433a9ceb34afc9
renpond <renpond12@gmail.com>
2023-02-16 17:59:53 -0600

    Update Meaning.js

--- project-dictionary/src/Meaning.js
+++ project-dictionary/src/Meaning.js
@@ -9,7 +9,6 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {" "}
               {definition.definition}
               <br />
               <em>{definition.example}</em>
