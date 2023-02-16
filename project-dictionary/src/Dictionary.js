694d67b9a1d26c992ef63f21e24dbc5b7f7fe9ac
renpond <renpond12@gmail.com>
2023-02-16 12:50:40 -0600

    Created search engine & started adding css

---
+++ project-dictionary/src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+///core concept to creating search function on a app/webpage
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    console.log(event.target.value);
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
